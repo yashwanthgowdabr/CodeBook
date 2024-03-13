7a939c872a37e8daf140b291cf3463ba9a275fc8
yashwanthgowdabr <85699717+yashwanthgowdabr@users.noreply.github.com>
2024-03-13 17:42:32 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,7 @@ export async function login(authDetail){
         headers: {"content-Type": "application/json"},
         body: JSON.stringify(authDetail)
     }
-    const response = await fetch("http://localhost:8000/login", requestOptions);
+    const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
     if(!response.ok){
         throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
@@ -24,7 +24,7 @@ export async function register(authDetail){
         headers: {"content-Type": "application/json"},
         body: JSON.stringify(authDetail)
     }  
-    const response = await fetch("http://localhost:8000/register", requestOptions);
+    const response = await fetch(`${process.env.REACT_APP_HOST}/register`, requestOptions);
     if(!response.ok){
         throw { message: response.statusText, status: response.status }; //eslint-disable-line
     }
